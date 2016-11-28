562cc03d3761f8c437da67018bd7107282a50cef
Arjun Adhia <aadhia@users.noreply.github.com>
2016-11-28 14:12:57 -0500

    Update .deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -10,8 +10,8 @@ cd site
 git init
 
 # Configure Git
-git config user.name "Travis CI"
-git config user.email "nbartlett7@gmail.com"
+git config user.name "Arjun Adhia"
+git config user.email "arjun.adhia@gmail.com"
 
 # Commit all the things into the repo
 git add .
